97ea3e3ebfec9843dcb1d49d17818521f2f91e49
Zhenhua Mao <zhenhua.mao@ranplan.net>
2017-07-18 10:00:12 +0800

    添加项目文件。

---
+++ CalcConvexPolygon/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CalcConvexPolygon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(676, 29);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "重置";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.btnReset);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ CalcConvexPolygon/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using MIConvexHull;
+
+namespace CalcConvexPolygon
+{
+    public partial class Form1 : Form
+    {
+        private Vertex[] _vertices;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            _vertices = null;
+            Invalidate();
+        }
+
+        private Vertex[] CreateVertices()
+        {
+            const int numberOfVertices = 100;
+            const double size = 500;
+
+            var r = new Random();
+            var vertices = new Vertex[numberOfVertices];
+            for (var i = 0; i < numberOfVertices; i++)
+                vertices[i] = new Vertex(size * r.NextDouble(), size * r.NextDouble());
+            return vertices;
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.TranslateTransform(10f, 10f);
+
+            if (_vertices == null)
+                _vertices = CreateVertices();
+
+            //绘制原始随机点组成的线条
+            for (var i = 1; i < _vertices.Length; i++)
+                g.DrawLine(Pens.BlueViolet, (float) _vertices[i - 1].Position[0], (float) _vertices[i - 1].Position[1],
+                    (float) _vertices[i].Position[0], (float) _vertices[i].Position[1]);
+
+            //计算凸包点
+            var result1 = ConvexHull.Create(_vertices);
+            var convexHull = result1.Points;
+
+            //对凸包点按该点与中心点组成的线与X轴的夹角进行排序，使凸包点列表组成一个多边形
+            var edgePoints = new List<PointF>();
+            edgePoints.AddRange(
+                convexHull.Select(cond => new PointF((float) cond.Position[0], (float) cond.Position[1])));
+            edgePoints.Sort(new AngleComparer(edgePoints));
+
+            //绘制凸包多边形
+            for (var i = 1; i <= edgePoints.Count; i++)
+            {
+                var pt1 = edgePoints[i - 1];
+                var pt2 = i == edgePoints.Count ? edgePoints[0] : edgePoints[i];
+                g.DrawLine(Pens.Green, pt1, pt2);
+                g.DrawString(i.ToString(), SystemFonts.CaptionFont, Brushes.Green, pt1.X + 8f, pt1.Y);
+            }
+        }
+
+
+        public class Vertex : IVertex
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Vertex" /> class.
+            /// </summary>
+            /// <param name="x"> The x position. </param>
+            /// <param name="y"> The y position. </param>
+            public Vertex(double x, double y)
+            {
+                Position = new[] {x, y};
+            }
+
+            public double[] Position { get; set; }
+        }
+
+        private class AngleComparer : IComparer<PointF>
+        {
+            private readonly PointF _center;
+
+            public AngleComparer(List<PointF> list)
+            {
+                //计算重心
+                var center = new PointF();
+                var x = 0f;
+                var y = 0f;
+                foreach (var pt in list)
+                {
+                    x += pt.X;
+                    y += pt.Y;
+                }
+                center.X = x / list.Count;
+                center.Y = y / list.Count;
+                _center = center;
+            }
+
+            public int Compare(PointF x, PointF y)
+            {
+                var angleX = GetAngle(_center, x);
+                var angleY = GetAngle(_center, y);
+
+                return Comparer<double>.Default.Compare(angleX, angleY);
+            }
+
+            private static double GetAngle(PointF p1, PointF p2)
+            {
+                if (p1.Equals(p2)) return 0.0;
+                var l = GetDistance(p1, p2);
+                if (p2.Y > p1.Y)
+                    return Math.Acos((p2.X - p1.X) / l);
+                return 2 * Math.PI - Math.Acos((p2.X - p1.X) / l);
+            }
+
+            private static double GetDistance(PointF p1, PointF p2)
+            {
+                var pt = new PointF(p2.X - p1.X, p2.Y - p1.Y);
+                return Math.Sqrt(pt.X * pt.X + pt.Y * pt.Y);
+            }
+        }
+    }
+}
